fc644f978bf0baf73c6b9a44ebf8530201e8eba1
PalomaC26 <carvalhodasilvapaloma133@gmail.com>
2024-04-11 10:44:06 -0300

    10 dia de javascript

---
+++ condicionais/exercicio.js
@@ -0,0 +1,7 @@
+var vel = 60
+console.log(`A velocidade do seu carro é ${vel}`)
+if(vel > 60){
+    console.log(`Voce ultrapassou a velocidade permitida, MULTADO!`)
+}
+
+console.log(`Dirija sempre usando o cinto de segurança`)
---
+++ condicionais/exercicio2.js
@@ -0,0 +1,8 @@
+var país = 'EUA'
+console.log(`Vivendo no ${país}`)
+if(país == 'Brasil'){
+    console.log('Voce é Brasileiro')
+}
+else {
+    console.log('Voce é Estrangeiro')
+}
